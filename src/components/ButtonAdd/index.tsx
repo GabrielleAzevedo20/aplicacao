3a2f2f59c977f318b9e01e29c8ed009af046bc52
GabrielleAzevedo20 <gabypedroso20@gmail.com>
2021-11-30 20:51:11 -0300

    segundaAvd

--- src/components/ButtonAdd/index.tsx
+++ src/components/ButtonAdd/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { TouchableOpacityProps } from 'react-native'
+import { Fontisto } from '@expo/vector-icons';
 import { Container, Icon } from './styles'
 
 
 export function ButtonAdd({ ...rest}: TouchableOpacityProps) {
     return (
         <Container {...rest}>
-            <Icon name='toggle-on' />
+            <Fontisto name="toggle-off" size={24} color="black" />
         </Container>
     )
 }
--- src/components/ButtonAdd/styles.ts
+++ src/components/ButtonAdd/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
 
 export const Container = styled.TouchableOpacity`
     height: 48px;
     width: 48px;
-    background-color: ${({ theme }) => theme.colors.overlay};
+    background-color: ${({ theme }) => theme.colors.heading};
     border-radius: 8px;
     align-items: center;
     justify-content: center;
