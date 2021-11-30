5b9c84cbc61d14e014afd35c75a39b0191811913
GabrielleAzevedo20 <gabypedroso20@gmail.com>
2021-11-30 20:52:44 -0300

    avd

--- src/components/ButtonAdd/styles.ts
+++ src/components/ButtonAdd/styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Fontisto } from '@expo/vector-icons';
 
 export const Container = styled.TouchableOpacity`
     height: 48px;
